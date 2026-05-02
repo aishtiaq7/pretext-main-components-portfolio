bd16f500f93a8762140a7feec425eff32ab75217
aishtiaq <awshafishtiaque@gmail.com>
2026-05-01 18:34:34 -0700

    revert all performance related updated on FLICKERING

--- src/components/ScrollInputs.tsx
+++ src/components/ScrollInputs.tsx
@@ -213,7 +213,7 @@ export function ScrollInputs({ shapes }: Props) {
         <input
           type="range"
           className="scroll-slider scroll-slider-z"
-          min={0.15}
+          min={0.25}
           max={3}
           step={0.01}
           value={zoom}
